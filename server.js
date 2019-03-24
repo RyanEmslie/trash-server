d0aa5c2f4655b3e8d7a378e92158be5070cea3df
ryan emslie <ryan.emslie@gmail.com>
2019-03-24 13:09:34 -0400

    cors added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const testData = require('./routes/api/testData');
 
@@ -8,7 +9,7 @@ const path = require('path');
 // initialize express
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // DB config
 const db = require('./config/keys').mongoURI;
 
